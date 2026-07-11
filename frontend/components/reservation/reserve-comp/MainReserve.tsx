f71c8894d865df3ea0434565cba6a85af78b29c7
XbrZey <hibreezo@gmail.com>
2026-07-11 22:36:05 +0545

    fixed some minor issues

--- frontend/components/reservation/reserve-comp/MainReserve.tsx
+++ frontend/components/reservation/reserve-comp/MainReserve.tsx
@@ -193,7 +193,7 @@ export default function MainReserve() {
             </div>
 
             <div className="space-y-2 md:col-span-2">
-              <label className="text-xs font-bold text-neutral-700 block">Preferred Time Slot</label>
+              
               <div className="grid grid-cols-2 sm:grid-cols-4 gap-3">
                 {['11:30 AM', '2:00 PM', '6:30 PM', '8:30 PM'].map((time) => (
                   <button
